cac78180665c511a10087c4d2f34fc8d03b21376
“J <793227936@qq.com>
2018-08-07 19:31:45 +0800

    test2

--- src/components/User/Function/NewProject.vue
+++ src/components/User/Function/NewProject.vue
@@ -10,27 +10,46 @@
         </el-steps>
         <el-button size="small" style="margin-top: 12px;" @click="next">下一步</el-button>
         <el-button size="small" style="margin-top: 12px;" @click="back">上一步</el-button>
-        <router-view></router-view>
+        <router-view class="input clearfix"></router-view>
     </div>
-    
 </template>
     
 <script>
   export default {
     data() {
       return {
-        active: 0
+        active: 0,
+        activeRouter:'step1'
       };
     },
 
     methods: {
-      next() {
-        if (this.active++ > 5) this.active = 0;
+      next() {  
+        if (++this.active > 5) {
+            this.active = 0;
+        }
+            this.activeRouter = 'step' + (this.active + 1)
+            this.$router.push({name:this.activeRouter})
       },
       back(){
           if(this.active-- == 0) this.active = 0;
+          this.activeRouter = 'step' + (this.active + 1)
+            this.$router.push({name:this.activeRouter})
       }
     }
   }
 </script>
+
+<style scoped>
+    .input{
+        width: 100%;
+        height: 430px;
+    }
+    .clearfix:after{
+        content:"";
+        display: block;
+        clear: both;
+    }
+</style>
+
 
